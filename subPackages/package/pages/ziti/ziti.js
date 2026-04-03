8aadb034e8121a24556dc5f5d0681ce4f3d064b0
Lgy-ikun <1215982109@qq.com>
2026-04-03 17:31:40 +0800

    门店选择添加位置电话等等，更改样式

--- subPackages/package/pages/ziti/ziti.js
+++ subPackages/package/pages/ziti/ziti.js
@@ -6,7 +6,8 @@ Page({
     stores: [],
     type: 'order',
     latitude: '',
-    longitude: ''
+    longitude: '',
+    isloading: true
   },
 
   onLoad(options) {
@@ -14,24 +15,115 @@ Page({
       type: options.type || 'order'
     });
 
-    const itsid = wx.getStorageSync('itsid');
-    if (itsid) {
-      this.getResult(itsid);
-    } else {
-      wx.showToast({
-        title: '登录信息已失效',
-        icon: 'none'
-      });
-    }
-
-    this.getLocation();
+    const itsid = wx.getStorageSync('itsid') || app.globalData.itsid || '';
+    this.getResult(itsid);
+    this.checkLocationPermission(false);
   },
 
   onShow() {
-    this.refreshStores();
+    this.checkLocationPermission(false);
   },
 
-  getLocation() {
+  clearLocationState() {
+    this.setData({
+      latitude: '',
+      longitude: ''
+    }, () => {
+      this.refreshStores();
+    });
+  },
+
+  getLocationFailureType(err = {}) {
+    const errMsg = String(err.errMsg || err.message || '').toLowerCase();
+
+    if (errMsg.includes('auth deny') || errMsg.includes('auth denied')) {
+      return 'authDenied';
+    }
+
+    if (
+      errMsg.includes('system permission denied') ||
+      errMsg.includes('location service disabled') ||
+      errMsg.includes('locationswitchoff') ||
+      errMsg.includes('location unavailable') ||
+      errMsg.includes('system not support location') ||
+      errMsg.includes('gps')
+    ) {
+      return 'serviceDisabled';
+    }
+
+    return 'other';
+  },
+
+  showLocationPermissionModal() {
+    if (this._hasShownLocationPermissionModal) {
+      return;
+    }
+
+    this._hasShownLocationPermissionModal = true;
+    wx.showModal({
+      title: '提示',
+      content: '需要您授权位置权限才能获取附近的门店，是否去设置开启？',
+      confirmText: '去设置',
+      cancelText: '取消',
+      success: (res) => {
+        if (!res.confirm) {
+          return;
+        }
+
+        wx.openSetting({
+          success: (settingRes) => {
+            if (settingRes.authSetting && settingRes.authSetting['scope.userLocation']) {
+              this.getLocation({
+                showSystemLocationPrompt: true
+              });
+            }
+          }
+        });
+      }
+    });
+  },
+
+  showLocationServiceDisabledModal() {
+    if (this._hasShownLocationServiceModal) {
+      return;
+    }
+
+    this._hasShownLocationServiceModal = true;
+    wx.showModal({
+      title: '提示',
+      content: '手机定位未开启，无法显示与店铺的距离，请先开启手机定位。',
+      showCancel: false,
+      confirmText: '我知道了'
+    });
+  },
+
+  checkLocationPermission(showPrompt = false) {
+    wx.getSetting({
+      success: (settingRes) => {
+        const authSetting = settingRes.authSetting || {};
+        if (authSetting['scope.userLocation'] === false) {
+          this.clearLocationState();
+          if (showPrompt) {
+            this.showLocationPermissionModal();
+          }
+          return;
+        }
+
+        this.getLocation({
+          showAuthPrompt: showPrompt,
+          showSystemLocationPrompt: showPrompt
+        });
+      },
+      fail: () => {
+        this.getLocation({
+          showAuthPrompt: showPrompt,
+          showSystemLocationPrompt: showPrompt
+        });
+      }
+    });
+  },
+
+  getLocation(options = {}) {
     wx.getLocation({
       type: 'wgs84',
       success: ({ latitude, longitude }) => {
@@ -42,13 +134,32 @@ Page({
           this.refreshStores();
         });
       },
-      fail: () => {
-        this.refreshStores();
+      fail: (err) => {
+        this.clearLocationState();
+
+        const failureType = this.getLocationFailureType(err);
+        if (failureType === 'authDenied') {
+          if (options.showAuthPrompt) {
+            this.showLocationPermissionModal();
+          }
+          return;
+        }
+
+        if (failureType === 'serviceDisabled') {
+          if (options.showSystemLocationPrompt) {
+            this.showLocationServiceDisabledModal();
+          }
+          return;
+        }
+
+        if (options.showSystemLocationPrompt) {
+          this.showLocationServiceDisabledModal();
+        }
       }
     });
   },
 
-  getResult(itsid) {
+  getResult(itsid = '') {
     wx.request({
       url: `${app.globalData.AUrl}/jy/go/we.aspx?ituid=106&itjid=10610&itcid=10626&itsid=${itsid}`,
       method: 'GET',
@@ -69,6 +180,11 @@ Page({
           title: '门店加载失败',
           icon: 'none'
         });
+      },
+      complete: () => {
+        this.setData({
+          isloading: false
+        });
       }
     });
   },
@@ -76,22 +192,16 @@ Page({
   refreshStores() {
     const selectedStoreId = String(wx.getStorageSync('selectedStoreId') || app.globalData.selectedStoreId || '');
     const sourceStores = Array.isArray(this.rawStores) ? this.rawStores : [];
-    const storesWithDistance = storeUtils.enrichStoresWithDistance(
+    const stores = storeUtils.enrichStores(
       sourceStores,
       this.data.latitude,
       this.data.longitude
-    );
-
-    const stores = storesWithDistance.map((store) => {
-      const businessTime = storeUtils.getStoreBusinessTime(store);
-      return {
-        ...store,
-        isSelected: String(store.id) === selectedStoreId,
-        displayAddress: storeUtils.getStoreAddress(store),
-        displayBusinessTime: businessTime || '以门店实际为准',
-        displayTag: storeUtils.getStoreTag(store)
-      };
-    });
+    ).map((store) => ({
+      ...store,
+      ctaText: store.isOpen ? '去下单' : '休息中',
+      ctaHintText: store.isOpen ? '选择门店' : '暂不可选',
+      isSelected: store.isOpen && String(store.id) === selectedStoreId
+    }));
 
     this.setData({ stores });
   },
@@ -99,7 +209,6 @@ Page({
   getStoreById(storeId) {
     const targetId = String(storeId || '');
     return (this.data.stores || []).find(store => String(store.id) === targetId)
-      || (this.rawStores || []).find(store => String(store.id) === targetId)
       || null;
   },
 
@@ -125,12 +234,21 @@ Page({
       return;
     }
 
+    if (!selectedStore.isOpen) {
+      wx.showToast({
+        title: '当前门店休息中，暂不可选',
+        icon: 'none'
+      });
+      return;
+    }
+
     app.globalData.selectedStoreName = selectedStore.name;
     app.globalData.storeName = selectedStore.name;
     app.globalData.selectedStoreId = storeId;
     app.globalData.selected = '自提';
     app.setStoreInfo && app.setStoreInfo(selectedStore);
     wx.setStorageSync('selectedStoreId', storeId);
+    wx.setStorageSync('selectedStoreName', selectedStore.name);
 
     wx.showToast({
       title: `已选择：${selectedStore.name}`,
